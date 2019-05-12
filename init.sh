8dd62363282ea3c672ed0c8e0855b22bcaf59b6a
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2019-05-12 09:05:49 +0900

    Delete unused file

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# decode2019のリソースグループを作成する
-az group --name decode2019 --location japaneast
-# リソースグループ上にKubernetesクラスターを作成する
-az aks create --resource-group decode2019 --name k8s-handson --node-count 1 --generate-ssh-keys
-# Kubernetesクラスターに接続するための認証情報を取得する
-az aks get-credentials --name k8s-handson --resource-group decode2019
